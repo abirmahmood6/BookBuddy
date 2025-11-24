ae764f69f8017fc65dfe3c716bc4c94eb4ed029b
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-11-24 00:12:46 -0500

    Set up frontend with Vite and Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-red-500"> Hello World </h1>
+    </div>
+  )
+}
+
+export default App;
+
+// justify-center → centers content horizontally.
+// items-center → centers content vertically.
