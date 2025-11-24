3dc77a845ed31d77272d89f6f79a0ff3614316c3
Abir M <102193884+abirmahmood6@users.noreply.github.com>
2025-11-24 04:19:30 -0500

    SPA and react setup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import CreateBook from "./pages/CreateBook"
+import EditBook from "./pages/EditBook"
+import ShowBook from "./pages/ShowBook"
+import DeleteBook from "./pages/DeleteBook"
+// { } = specific things you pick from a library
+// No braces { } = one main thing 
+
 const App = () => {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-red-500"> Hello World </h1>
+    <div> {/* Add the DaisyUI theme here */}
+      <Routes>
+        <Route path="/" element={<HomePage/>}/> 
+        <Route path="/books/create" element={<CreateBook/>}/> 
+        <Route path="/books/details/:id" element={<ShowBook/>}/> 
+        <Route path="/books/edit/:id" element={<EditBook/>}/> 
+        <Route path="/books/delete/:id" element={<DeleteBook/>}/> 
+      </Routes>
     </div>
   )
 }
